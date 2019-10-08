76ac57616a78324428603c0d8c8430c5fb33e75f
Joo YeJin Kelly <yejinkellyjoo@gmail.com>
2019-10-08 19:09:19 +0900

    turtle attempt#1, fail

--- 2959_turtle/main.cpp
+++ 2959_turtle/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
+void selectionSort(int size, int arr[]){
+    int smallest;
+    for (int i = 1; i <= size-1 ; ++i) {
+        smallest = i;
+        for (int j = i+1; j <= size; ++j) {
+            if (arr[j] < arr[smallest])
+                smallest = j;
+        }
+        int tmp = arr[i];
+        arr[i] = arr[smallest];
+        arr[smallest] = tmp;
+    }
+    std::cout << arr[0] << arr[1] << arr[2] << arr[3];
+
+    std::cout << arr[0] * arr[2];
+
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int arr[4] = {0, };
+    std::cin >> arr[0] >> arr[1] >> arr[2] >> arr[3];
+
+    selectionSort(4, arr);
+
     return 0;
 }
