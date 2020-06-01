0844ba8544e6d4e0c40896343b52879819975c33
Joo YeJin Kelly <yejinkellyjoo@gmail.com>
2020-06-02 01:43:35 +0900

    adjusted the code to the assignment requirements

--- 2020-1_hw2/main.cpp
+++ 2020-1_hw2/main.cpp
@@ -13,51 +13,54 @@ int bigger_value(int a, int b){
 
 
 int main() {
-    int num_of_tests, arr_size, best_start, best_end;
-    signed int *arr, *sum, *best;
+    int arr_size, best_start, best_end;
+    signed int *arr, *sum, best;
 
-    scanf("%d", &num_of_tests);     // receive number of tests
-    best = new signed int[num_of_tests];    // allocate best array with the size of number of tests
-    for (int j = 0; j < num_of_tests; ++j) {    // for each test,
 
-        scanf("%d", &arr_size);         // receive array size
-        arr = new signed int[arr_size];         // allocate array with according array size
+    scanf("%d", &arr_size);         // receive array size
+    arr = new signed int[arr_size];         // allocate array with according array size
+    sum = new signed int[arr_size];         // allocate sum with the same size
 
-        for (int i = 0; i < arr_size; i++) {    // fill in the array
-            scanf("%d", &arr[i]);
+    for (int i = 0; i < arr_size; i++) {    // fill in the array
+        scanf("%d", &arr[i]);
+    }
+    fflush(stdin);
+
+    // copy arr values to sum
+    for (int i = 0; i < arr_size; ++i) {
+        sum[i] = arr[i];
+    }
+
+
+    // 1. find the biggest number in the array. This is the best answer for now.
+    best = arr[0];
+    best_start = best_end = 0;
+    for (int i = 1; i < arr_size; i++) {
+        if (bigger_value(best, arr[i]) != best){
+            best = bigger_value(best, arr[i]);
+            best_start = best_end = i;
         }
-        fflush(stdin);
+    }
 
-        // 1. find the biggest number in the array. This is the best answer for now.
-        best[j] = arr[0];
-        for (int i = 1; i < arr_size; i++) {
-
-            best[j] = bigger_value(best[j], arr[i]);
-
-        }
-
-
-        sum = new signed int[arr_size];
-        for (int i = 0; i < arr_size; ++i) {    // copy arr values to sum
-            sum[i] = arr[i];
-        }
-
-        for (int rounds = 1; rounds < arr_size; rounds++) {
-            for (int sum_loc = 0; sum_loc < arr_size - rounds; sum_loc++) {
-                sum[sum_loc] = sum[sum_loc] + arr[sum_loc+rounds];
-                best[j] = bigger_value(sum[sum_loc], best[j]);
+    // 2. The DP algorithm starting from level 2.
+    for (int rounds = 1; rounds < arr_size; rounds++) {
+        for (int sum_loc = 0; sum_loc < arr_size - rounds; sum_loc++) {
+            sum[sum_loc] = sum[sum_loc] + arr[sum_loc+rounds];
+            if (bigger_value(best, sum[sum_loc]) != best){
+                best = bigger_value(sum[sum_loc], best);
+                best_start = sum_loc;
+                best_end = sum_loc + rounds;
             }
         }
-
-        delete[] sum;
-        delete[] arr;
     }
+
+    delete[] sum;
+    delete[] arr;
 
     // answer!
-    for (int j = 0; j < num_of_tests; ++j) {
-        printf("%d\n", best[j]);
+    for (int j = best_start; j <= best_end; ++j) {
+        printf("%d ", arr[j]);
     }
-    delete[] best;
 
     return 0;
 }
